cd468d940ba49eeb8678a4b1d565f4859d8beed6
Koichi98 <kotty.0704@gmail.com>
2022-02-03 21:55:25 +0900

    step1 preparetion

--- driver/null.c
+++ driver/null.c
@@ -1,4 +1,10 @@
-#include "null.h"
+#include <stdio.h>
+#include <stdint.h>
+
+#include "util.h"
+#include "net.h"
+
+#define NULL_MTU UINT16_MAX  /*maximum size of IP datagram */
 
 
 static net_device * null_init(void){
@@ -9,3 +15,7 @@ static net_device * null_init(void){
 static int null_transmit(){
 
 }
+
+static struct net_device_ops null_ops = {
+    .transmit = null_transmit,
+};
